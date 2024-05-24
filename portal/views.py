f6512bd6b371dd23d455a94900c631c1b49099ff
Frandey Briceño <f.briceno@correo.unimet.edu.ve>
2024-05-24 10:01:24 -0400

    Session update, registro update

--- portal/views.py
+++ portal/views.py
@@ -10,8 +10,10 @@ def inicio(request):
     data=request.GET
     magic = request.GET['magic']
     post = request.GET['post']
-    print(magic)
-    print(post)
+    request.session["magic"] = magic
+    request.session["post"] = post
+    print(request.session["magic"])
+    print(request.session["post"])
     if request.method=="GET":
         print("Iniciando sesión")
         return render(request,"login.html",{ 
@@ -26,7 +28,7 @@ def inicio(request):
 
 
 def registro(request):
-    url = "https://10.10.10.1:39443/"
+    """url = "https://10.10.10.1:39443/"
 
     payload = "username=admin&secretkey=#5C0ntel6&ajax=1"
     headers = {
@@ -36,8 +38,19 @@ def registro(request):
     response = requests.request("POST", url, headers=headers, data=payload, verify=False)
 
     print(response.text)
-    print("registro")
-
+    print("registro")"""
+    
+    if request.method=="GET":
+        print("Se obtiene para la creación del usuario Guest")
+        return render(request,"registro_guest.html")
+    else:
+        data=request.POST
+        correo = data['correo']
+        print(correo)
+        return render(request,"login.html",{ 
+            'magic':magic,
+            'post':post,
+            })
 
 def login(request):
     print("login")
