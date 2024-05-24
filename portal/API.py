d458c21e245272f68046857966f51e4ea337e6b2
FrandaContel <frandeybriceno@contelonline.com>
2024-05-24 13:19:52 -0400

    guest update

--- portal/API.py
+++ portal/API.py
@@ -114,9 +114,11 @@ class Fortigate:
         response = requests.request("GET", url, headers=self.headers, data=self.payload, verify=False)
         response = json.loads(response.text)
         print(httpStatus(response))
-        for i in range (0,len(response['results'][0]['member'])):
-            nombres.append(response['results'][0]['member'][i]['name'])
-        
+        print(response['results'][0]['guest'])
+        for i in range (0,len(response['results'][0]['guest'])):
+            print(response['results'][0]['guest'][i]['expiration'])
+            nombres.append(response['results'][0]['guest'][i]['user-id'])
+        print(nombres)
         return nombres
 
     def CreateGroup(self):
@@ -171,11 +173,13 @@ class Fortigate:
             response = json.loads(response.text)
             #Se itera por los elementos de la lista "results", donde cada elemento contiene la informacion de un usuario
             print(response)
-            members_info = response['results'][0]['member'] #Se agregan los nombres de los usuarios a una lista vacia
+            members_info = response['results'][0]['guest'] #Se agregan los nombres de los usuarios a una lista vacia
             for i in range(0, len(members_info)):
-                members.append(members_info[i]["name"])
+                if (members_info[i]['expiration']=='3600'):
+                    members.append(members_info[i]["user-id"])
             print(members)
-            return members
+            print("lol")
+            return members[0]
     
     def AddUserToGroup(self, group, correo):
         url = self.api_url + f'cmdb/user/group/{group}/guest'
@@ -183,6 +187,7 @@ class Fortigate:
         self.payload = {'json':
                         {
                         'email':correo,
+                        'password':'fortinet',
                         'expiration':3600
                         }
                         }
