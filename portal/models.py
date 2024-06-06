8ba6325087616a1e8293aa18fc54e2ca2a2afa0d
FrandaContel <frandeybriceno@contelonline.com>
2024-06-06 09:59:32 -0400

    Update
    Video update, macaddress models

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.utils.timezone import now
 # Create your models here.
+
+class MAC_log(models.Model):
+    macvalue = models.CharField(max_length=20)
+    hora = models.DateTimeField(default=now)
+
+    def __str__(self) -> str:
+        return super().__str__(self.macvalue + ' ' + self.hora)
